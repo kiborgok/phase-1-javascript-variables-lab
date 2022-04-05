4d4211fa356b7b66b793d2f1ff3586727e9b6664
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-05 14:01:28 +0300

    add tests to index page

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const companyName = 'Scuber';
+const companyName = "Scuber";
 
-let mostProfitableNeighborhood = 'Chelsea';
+let mostProfitableNeighborhood = "Chelsea";
 
-let companyCeo = 'Susan Smith';
+let companyCeo = "Susan Smith";
+
+const mochaTests = document.getElementById("mocha");
+
+fetch("./results.json")
+  .then((res) => res.json())
+    .then((res) => {
+      res.tests.forEach((test) => {
+          const titleNode = document.createElement("h1");
+          const fullTitleNode = document.createElement("h2");
+          const titleTextNode = document.createTextNode(`${test.title}`);
+          const fullTitleTextNode = document.createTextNode(`${test.fullTitle}`);
+          titleNode.appendChild(titleTextNode);
+          fullTitleNode.appendChild(fullTitleTextNode);
+          mochaTests.appendChild(titleNode);
+          mochaTests.appendChild(fullTitleNode);
+    });
+  });
